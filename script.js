2585f9efb5a16ae86fee3090cd7517e31427250f
Maria Clarita Osorio <mariaclaritaosorio@MacBook-Pro-de-Claris.local>
2023-03-23 01:45:09 -0600

    mapa agregado

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:19.42847 ,lng: -99.12766};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
